3cce1fa8df8a6aa9c268d1c0eb02a45c368a1fc0
Wuzhiyin <979446886@qq.com>
2020-10-19 21:01:02 +0800

    step-2 配置express-art-template模板

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.use('/public/', express.static(path.join(__dirname, './public/')))
 
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
 
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views/'))
+
 app.get('/', function (req, res) {
-	res.send('index.html')
+	res.render('index.html')
 })
 
 app.listen(5000, function () {

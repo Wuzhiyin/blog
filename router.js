5de1bd0a448ed1df64cb941fba3309b617ad17fa
Wuzhiyin <979446886@qq.com>
2020-10-24 07:40:22 +0800

    step-11 退出功能实现

--- router.js
+++ router.js
@@ -115,6 +115,14 @@ router.post('/register', function (req, res) {
         })
     })
 })
+
+router.get('/logout', function (req, res) {
+    // 清除登陆状态
+    req.session.user = null
+
+    // 重定向到登录页
+    res.redirect('/login')
+})
 // router.post('/register', async function (req, res) {
 //   var body = req.body
 //   try {

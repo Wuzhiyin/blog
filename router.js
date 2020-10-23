70df55a4a7c97533a2f60c9f92e0a7541abfbdd2
Wuzhiyin <979446886@qq.com>
2020-10-24 07:38:10 +0800

    step-10 完成登录功能

--- router.js
+++ router.js
@@ -11,6 +11,45 @@ router.get('/', function (req, res) {
     })
 })
 
+router.get('/login', function (req, res) {
+    res.render('login.html')
+})
+
+router.post('/login', function (req, res) {
+    // 1. 获取表单数据
+    // 2. 查询数据库用户名密码是否正确
+    // 3. 发送响应数据
+    var body = req.body
+
+    User.findOne({
+        email: body.email,
+        password: md5(md5(body.password))
+    }, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: err.message
+            })
+        }
+
+        // 如果邮箱和密码匹配，则 user 是查询到的用户对象，否则就是 null
+        if (!user) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Email or password is invalid.'
+            })
+        }
+
+        // 用户存在，登陆成功，通过 Session 记录登陆状态
+        req.session.user = user
+
+        res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
+})
+
 router.get('/register', function (req, res) {
     res.render('register.html')
 })
